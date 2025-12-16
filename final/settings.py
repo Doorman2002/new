e1a677c896d348bc6b7f537b0fb464cc7b2f4791
Elvis <okorieonyekachi003@gmail.com>
2025-12-16 22:15:05 +0100

    Update Django ALLOWED_HOSTS

--- final/settings.py
+++ final/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-xxrwyrde(@gqulo2nqbexir5)w2kss0txl=ns_sz5h6sd!bdi^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "new-proj-qqor.onrender.com",
+    ".onrender.com",
+]
+
 
 
 # Application definition
